9ccd4a9575949b8aeb7f076a05d8ec5969cff01c
bashirovabduaziz <bashirovabduaziz66@gmail.com>
2023-06-12 14:41:17 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// 1
+
+//  function powerA(a,b,c,d,e){
+//    console.log(a**2,b**2,c**2,d**2,e**2);
+//  }
+//  powerA(0,2,3,4,5)
+
+// 2
+
+// function powerA234(a,b,c){
+//   console.log(a**2,a**3,a**4,b**2,b**3,b**4,c**2,c**3,c**4);
+// }
+// powerA234(2,3,4)
+
+// 3
+
+// function MEAN(a,b,c){
+//   console.log((a+b%2),(a+c%2));
+// }
+// MEAN(2,4,6)
+
+// 4
+
+// function TRIANGLE(a,a,a){
+//     console.log(a+a+a);
+// }
+// TRIANGLE(2,2,2)
+
+// 5
+
+// function RectPc(x1,y1,x2,y2){
+//     console.log(x1+x2+y1+y2);
+//     console.log(x1*y1);
+// }
+// RectPc(2,3,2,3)
+
+// 6
+
+// function DigitCountSum(a,b,c){
+//     console.log(a+b+c);
+// }
+// DigitCountSum(2,5,8)
+
+// 17
+
+// function ildiz(a,b,c,x){
+//     console.log(a*x**2+b*x+c);
+// }
+// ildiz(2,3,2,3)
